a51f58b0e678849379b18370a69d5d5dfd73d753
Cleiton <cleiton4055@hotmail.com>
2025-10-21 14:40:22 -0300

    Alterações

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,48 +1,68 @@
-const output = document.getElementById("output")
-const n1 = document.getElementById("n1")
-const n2 = document.getElementById("n2")
-
-function somar(){
-    const n1Value = Number(n1.value)
-    const n2Value = Number(n2.value)
-
-    const resultado = n1Value + n2Value
-
-    output.innerHTML = `${resultado}`
+const limparCampos = () =>{
+    document.getElementById("n1").value = ""
+    document.getElementById("n2").value = ""
 }
 
-function diminuir(){
-    const n1Value = Number(n1.value)
-    const n2Value = Number(n2.value)
+const mostrarAlert = (mensagem) =>{
+    const alert = document.getElementById("alert")
+    const alertText = document.getElementById("alert-text")
 
-    const resultado = n1Value - n2Value
+    alertText.innerText = mensagem
+    alert.style.display = "block"
 
-    output.innerHTML = `${resultado}`
+    setTimeout(() =>{
+        closedAlert()
+    }, 2500)
 }
 
-function multiplicar(){
-    const n1Value = Number(n1.value)
-    const n2Value = Number(n2.value)
-
-    const resultado = n1Value * n2Value
-
-    output.innerHTML = `${resultado}`
+const closedAlert = () =>{
+    document.getElementById("alert").style.display = "none"
 }
 
-function dividir(){
-    const n1Value = Number(n1.value)
-    const n2Value = Number(n2.value)
+const calculo = () => {
+    const operacao = document.getElementById("operacao").value
+    const n1Value = Number(document.getElementById("n1").value)
+    const n2Value = Number(document.getElementById("n2").value)
+    const output = document.getElementById("output")
 
-    const resultado = n1Value / n2Value
+    if(!operacao){
+        mostrarAlert("Por favor, selecione uma operacao!")
+        return
+    }
 
-    output.innerHTML = `${resultado}`
-}
+    if(!n1Value || !n2Value){
+        mostrarAlert("Por favor, digite um número nos campos!")
+        return
+    }
 
-function porcentagem(){
-    const n1Value = Number(n1.value)
-    const n2Value = Number(n2.value)
+    let resultado = 0
 
-    const resultado = (n1Value / 100) * n2Value
+    switch(operacao){
+        case "somar":
+            resultado = n1Value + n2Value
+            break
+        case "diminuir":
+            resultado = n1Value - n2Value
+            break
+        case "multiplicar":
+            resultado = n1Value * n2Value
+            break
+        case "dividir":
+            if(n2Value === 0){
+                mostrarAlert("Não podemos dividir por 0!")
+                return
+            }else{
+                resultado = n1Value / n2Value
+            }
+            break
+        case "porcentagem":
+            resultado = (n1Value / 100) * n2Value
+            break
+        default:
+            mostrarAlert("Operação inválida!")
+            return
+    }
 
-    output.innerHTML = `${resultado}`
+    output.innerHTML = `Result: ${resultado.toFixed(2)}`
+    limparCampos()
 }
